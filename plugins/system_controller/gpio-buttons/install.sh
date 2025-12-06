176dc1f5eacd4d425eee035851671c15928f1392
Michele Perniola <michele.perniola@gmail.com>
2025-12-07 00:12:46 +0100

    Added Stop Button

--- plugins/system_controller/gpio-buttons/install.sh
+++ plugins/system_controller/gpio-buttons/install.sh
@@ -4,10 +4,10 @@ echo "Initializing config"
 
 # compatibilty with earlier config files due to few commands name change and directory rename
 if [ -f /data/configuration/miscellanea/gpio-buttons/config.json ];then
-	mv /data/configuration/miscellanea/gpio-buttons /data/configuration/system_controller/gpio-buttons
-	sed -i 's/playpause/playPause/g' /data/configuration/system_controller/gpio-buttons/config.json
-	sed -i 's/volup/volumeUp/g' /data/configuration/system_controller/gpio-buttons/config.json
-	sed -i 's/voldown/volumeDown/g' /data/configuration/system_controller/gpio-buttons/config.json
+	mv /data/configuration/miscellanea/gpio-buttons /data/configuration/system_controller/gpio-buttons-mod
+	sed -i 's/playpause/playPause/g' /data/configuration/system_controller/gpio-buttons-mod/config.json
+	sed -i 's/volup/volumeUp/g' /data/configuration/system_controller/gpio-buttons-mod/config.json
+	sed -i 's/voldown/volumeDown/g' /data/configuration/system_controller/gpio-buttons-mod/config.json
 fi
 
 echo "plugininstallend"
